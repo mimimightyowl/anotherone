500effeda0048bce7e96859de430d3e458b97361
mghtwl <63245559+mghtwl@users.noreply.github.com>
2021-06-22 15:13:30 +0400

    Add files via upload

---
+++ root/components/header.php
@@ -0,0 +1,38 @@
+<header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-xxl">
+              <a class="navbar-brand" href="">Панель навигации</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/index.php">Главная</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/first/first.php">Первая</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/second/second.php">Вторая</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/third/third.php">Третья</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/fourth/fourth.php">Четвёртая</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/fifth/fifth.php">Пятая</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/sixth/sixth.php">Шестая</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/seventh/seventh.php">Седьмая</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </header>
