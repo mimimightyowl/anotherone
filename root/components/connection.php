f3a29ac429d5a91e8f6e97ed2ac1509fc16f1a4b
mghtwl <63245559+mghtwl@users.noreply.github.com>
2021-06-22 15:26:22 +0400

    Delete root directory

--- root/components/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost'; // адрес сервера 
-$user = 'root'; // имя пользователя
-$password = 'root'; // пароль
-// $page = isset($_GET['page']) ? $_GET['page'] : 1;
-// $limit = 5;
-// $offset = $limit * ($page - 1);
-?>
